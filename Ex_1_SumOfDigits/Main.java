2cc794a24102946fb9068a37bb35a4069f53bffe
ulaganathanms87 <ulaganathan.cce@sairamit.edu.in>
2026-01-30 22:16:17 +0530

    Update Main.java

--- Ex_1_SumOfDigits/Main.java
+++ Ex_1_SumOfDigits/Main.java
@@ -1,1 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO: Read a positive integer from user
+        // TODO: Find sum of individual digits
+        // TODO: Display the result
+    }
+}
 
